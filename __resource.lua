6c9763eecbd743a33cec4c5a472eae984f3b034e
matheuh <57770561+matheuh@users.noreply.github.com>
2020-02-24 21:54:38 +0100

    Edit some shit part2

--- __resource.lua
+++ __resource.lua
@@ -7,6 +7,25 @@ data_file 'DLC_ITYP_REQUEST' 'ext_veg.ytyp'
 
 file 'int_veg.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'int_veg.ytyp'
+file 'props_km.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_km.ytyp'
+file 'props_ah.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_ah.ytyp'
+
+file 'int_door.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'int_door.ytyp'
+file 'ext_door.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'ext_door.ytyp'
+file 'props_ab.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_ab.ytyp'
+
+file 'props_ld.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_ld.ytyp'
+file 'props_mp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_mp.ytyp'
+file 'props_ss.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'props_ss.ytyp'
+
 
 file 'minigame.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'minigame.ytyp'
