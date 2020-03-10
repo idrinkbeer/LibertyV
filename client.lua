4ca9531ef8deb16bee624e526ff1a0719b3e5124
AntwanR942 <46630572+AntwanR942@users.noreply.github.com>
2020-03-10 22:50:43 +0000

    Optimisation

--- client.lua
+++ client.lua
@@ -1,92 +1,32 @@
 
-local x,y,z = 1198.015, 420.312, 231.624
-
 Citizen.CreateThread(function()
+	local Models = {
+		"freight",
+		"freightcar",
+		"freightgrain",
+		"freightcont1",
+		"freightcont2",
+		"freighttrailer",
+		"tankercar",
+		"metrotrain",
+		"s_m_m_lsmetro_01"
+	}
 
+	for _, Model in pairs(Models) do
+		ModelHash = GetHashKey(Model)
+		while not HasModelLoaded(ModelHash) do
+			RequestModel(ModelHash)
+			Citizen.Wait(1)
+		end
+	end
 
-function LoadTrainModels() -- f*ck your rails, too!
-			tempmodel = GetHashKey("freight")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("freightcar")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("freightgrain")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("freightcont1")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("freightcont2")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("freighttrailer")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
+	for i = 13, 17, 1 do
+		SwitchTrainTrack(i, true) 
+		SetTrainTrackSpawnFrequency(i, 12000)    
+	end
 
-			tempmodel = GetHashKey("tankercar")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("metrotrain")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-			tempmodel = GetHashKey("s_m_m_lsmetro_01")
-			RequestModel(tempmodel)
-			while not HasModelLoaded(tempmodel) do
-				RequestModel(tempmodel)
-				Citizen.Wait(0)
-			end
-			
-		
-	end		
+	SetRandomTrains(1)
 end)
 
-Citizen.CreateThread(function()
-print("HEeee")
 
-  SwitchTrainTrack(13, true)
-  SwitchTrainTrack(14, true)
-  SwitchTrainTrack(15, true)
-  SwitchTrainTrack(16, true)  
-  SwitchTrainTrack(17, true)    
-
-  SetTrainTrackSpawnFrequency(13, 12000)
-  SetTrainTrackSpawnFrequency(14, 12000)
-  SetTrainTrackSpawnFrequency(15, 12000)
-  SetTrainTrackSpawnFrequency(16, 12000)  
-  SetTrainTrackSpawnFrequency(17, 12000)    
-  SetRandomTrains(1)
-  
-end)
 
